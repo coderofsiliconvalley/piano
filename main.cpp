726085435c836ad475c29452ff81a82eee5cac99
Przemyslaw Czarnota <x2n404@gmail.com>
2016-04-29 02:14:02 +0200

    Added basic edge detection

--- main.cpp
+++ main.cpp
@@ -4,7 +4,28 @@
 
 int main(int argc, char ** argv) 
 {
-    cv::namedWindow("Hello world", cv::WINDOW_AUTOSIZE);
+    auto video_capture = cv::VideoCapture {"video.mp4"};
+
+    if (!video_capture.isOpened()) {
+        std::cout << "Couldn't open video.mp4" << std::endl;
+        return 0;
+    }
+
+    cv::namedWindow("main", cv::WINDOW_AUTOSIZE | 1);
+
+    while (cv::waitKey(30) < 0) {
+        auto frame = cv::Mat {};
+
+
+        if (!video_capture.read(frame)) {
+            break;
+        }
+
+        cv::Canny(frame, frame, 50, 200, 3);
+
+        imshow("main", frame); 
+    }
+
     cv::waitKey(0);
     return 0;
 }
